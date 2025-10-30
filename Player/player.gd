1372be8cfec896f00bea85f63d8326f543878e3a
Solange <solange.le-luel@epitech.eu>
2025-10-30 12:54:35 +0100

    :sparkles: - Detect nearest enemy

---
+++ Player/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+ 
+var speed : float = 150
+var health : float = 100.0 :
+	set(value):
+		health = value
+		%Health.value = value
+ 
+var nearest_enemy : CharacterBody2D
+var nearest_enemy_distance : float = INF
+
+func _physics_process(delta):
+	if nearest_enemy:
+		nearest_enemy_distance = nearest_enemy.separation
+		print(nearest_enemy.name)
+	else:
+		nearest_enemy_distance = INF
+	
+	velocity = Input.get_vector("left","right","up","down") * speed
+	move_and_collide(velocity * delta)
+ 
+func take_damage(amount):
+	health -= amount
+	print(amount)
+ 
+func _on_self_damage_body_entered(body):
+	take_damage(body.damage)
