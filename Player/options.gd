96eb58d379b59091005a2439109121c5a028d061
Meunier Hugo <hugohm5510@gmail.com>
2025-10-31 12:32:02 +0100

    all

--- Player/options.gd
+++ Player/options.gd
@@ -8,27 +8,27 @@ var OptionSlot = preload("res://UI/Options/option_slot.tscn")
 func _ready():
 	hide()
 	particles.hide()
-	
+
 func close_option():
 	hide()
 	particles.hide()
 	get_tree().paused = false
-	
+
 func get_available_weapons():
 	var weapons_resource = []
 	for weapon in weapons.get_children():
 		if weapon.weapon != null:
 			weapons_resource.append(weapon.weapon)
 	return weapons_resource
-	
+
 func show_options():
 	var weapons_available = get_available_weapons()
 	if weapons_available.size() == 0:
 		return
-		
+
 	for slot in get_children():
 		slot.queue_free()
-	
+
 	var option_size = 0
 	for weapon in weapons_available:
 		if weapon.is_upgradable():
@@ -36,10 +36,10 @@ func show_options():
 			option_slot.weapon = weapon
 			add_child(option_slot)
 			option_size += 1
-	
+
 	if option_size == 0:
 		return
-		
+
 	show()
 	particles.show()
 	get_tree().paused = true
